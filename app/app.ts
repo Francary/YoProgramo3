42f4b505764df28c49d0dedfed108fd1323a9968
Francary <francarymolinapaez@gmail.com>
2022-10-12 23:08:20 -0300

    Practica12102022

--- app/app.ts
+++ app/app.ts
@@ -1,8 +1,8 @@
 let message: string ='Hello World Probando 3.0'
 console.log(message);
 let nombre: string = "Mateo";
-const mensaje = `Mi nombre es ${nombre} Soy nuevo en TypeScript.`;
-console.log(mensaje)
+const mensajePractica = `Mi nombre es ${nombre} Soy nuevo en TypeScript.`;
+console.log(mensajePractica)
 
 let nombre2: string = "Francary Molina";
 let mensaje1: string = `Mi nombre es ${nombre2} Estoy practicando TypeScript`;
@@ -36,3 +36,171 @@ class Persona {
 
     }
    }
+
+   // Uso de una Super Clase 
+
+   class Operacion {
+    protected valorA:number;
+    protected valorB:number;
+    protected resultado:number;
+    constructor (){
+        this.valorA=0;
+        this.valorB=0;
+        this.resultado=0;
+    }
+    set ValorA(value:number) {
+        this.valorA=value;
+    }
+    set ValorB(value:number) {
+        this.valorB=value;
+    }
+    get Resultado ():number{
+        return this.resultado;
+    }
+   }
+
+   class Suma extends Operacion
+   {
+    Sumar ()
+    {
+        this.resultado=this.valorA+this.valorB;
+    }
+   }
+   class Resta extends Operacion
+   {
+    Restar ()
+    {
+        this.resultado=this.valorA-this.valorB;
+    }
+   }
+
+   let opercaionS= new Suma();
+   opercaionS.ValorA=45;
+   opercaionS.ValorB=10;
+   opercaionS.Sumar();
+   console.log("El resultado de la suma es "+opercaionS.Resultado);
+   alert("El resultado de la suma es "+opercaionS.Resultado)
+
+   let opercaionR= new Resta();
+   opercaionR.ValorA=45;
+   opercaionR.ValorB=10;
+   opercaionR.Restar();
+   console.log("El resultado de la resta es "+opercaionR.Resultado);
+   alert("El resultado de la resta es "+opercaionR.Resultado);
+
+// Practica de Aereoplano
+
+class Turbina
+{
+    private numTurbinas:number = 0;
+    public constructor(n :number)
+    {
+        this.numTurbinas = n;
+    }
+    public ToString()
+    {
+        return this.numTurbinas +" Turbina/s";
+    }
+}
+
+
+class Cubierta
+{
+    private cabinaTripulacion:boolean = false;
+    private cabinaVuelo:boolean = false;
+    private sistemaEmergencia:boolean = false;
+    private numTanquesCombustible:number = 4;
+    private numPuestasSalidas:number = 4;
+
+    public constructor( pCabinaTripulacion:boolean, pCabinaVuelo:boolean, pSistemaEmergencia:boolean, pTanquesCombustible:number, pPuertasSalida:number)
+
+    {
+        this.cabinaTripulacion = pCabinaTripulacion;
+        this.cabinaVuelo = pCabinaVuelo;
+        this.sistemaEmergencia = pSistemaEmergencia;
+        this.numTanquesCombustible = pTanquesCombustible;
+        this.numPuestasSalidas = pPuertasSalida;
+    }
+    
+    public ToString()
+    {
+        let mensaje = "Cubierta compuesta de: " ;
+        if( this.cabinaVuelo)
+        {
+            mensaje += " cubierta de Vuelo, ";
+        }
+        if(this.cabinaTripulacion)
+        {
+            mensaje += " cubierta de Tripulación, ";            
+        }
+        if(this.sistemaEmergencia)
+        {
+            mensaje += " Sistema de Emergencia, ";
+        }
+            mensaje += this.numTanquesCombustible + " Tanques de Combustible, ";
+            mensaje += this.numPuestasSalidas + " Puertas de Salida. ";
+        return mensaje;    
+    }
+}
+
+class Helice
+{
+    private numHelices:number = 1;
+    public constructor(n :number)
+    {
+        this.numHelices = n;
+    }
+    public ToString()
+    {
+        return this.numHelices +"  Helices";
+    }
+}
+
+class Alas
+{
+    private numAla:number = 2;
+    public constructor(n :number)
+    {
+        this.numAla = n;
+    }
+    public ToString()
+    {
+        return this.numAla +"  Las Alas";
+    }
+}
+class TrenDeAterrizaje
+{
+    private numTrenAterrizaje:number = 3;
+    public constructor(n :number)
+    {
+        this.numTrenAterrizaje = n;
+    }
+    public ToString()
+    {
+        return this.numTrenAterrizaje +"  Los Trenes";
+    }
+}
+
+class Aereoplano
+{
+    private helice: Helice ;
+    private trenAterrizaje: TrenDeAterrizaje ;
+    private alas: Alas ;
+    private cubierta:Cubierta;
+    constructor( phelice:Helice, pTrenAterrizaje:TrenDeAterrizaje, pAlas:Alas, pCubierta:Cubierta)
+    {
+        this.helice = phelice;
+        this.trenAterrizaje = pTrenAterrizaje;
+        this.alas = pAlas;
+        this.cubierta = pCubierta;
+    }
+    public ToString()
+    {
+        let mensaje = " Aeroplano compuesto por: ";
+        mensaje += this.helice.ToString();
+        mensaje += this.alas.ToString();
+        mensaje += this.trenAterrizaje.ToString();
+        mensaje += this.cubierta.ToString();
+        return mensaje;
+    }
+}
--- output/app.js
+++ output/app.js
@@ -12,8 +12,8 @@ let recursos = ['memoria', 'disco', 'procesador'];
 let message = 'Hello World Probando 3.0';
 console.log(message);
 let nombre = "Mateo";
-const mensaje = `Mi nombre es ${nombre} Soy nuevo en TypeScript.`;
-console.log(mensaje);
+const mensajePractica = `Mi nombre es ${nombre} Soy nuevo en TypeScript.`;
+console.log(mensajePractica);
 let nombre2 = "Francary Molina";
 let mensaje1 = `Mi nombre es ${nombre2} Estoy practicando TypeScript`;
 console.log(mensaje1);
@@ -37,3 +37,124 @@ let Persona2 = class Persona2 {
 Persona2 = __decorate([
     DecoradorPersona
 ], Persona2);
+// Uso de una Super Clase 
+class Operacion {
+    constructor() {
+        this.valorA = 0;
+        this.valorB = 0;
+        this.resultado = 0;
+    }
+    set ValorA(value) {
+        this.valorA = value;
+    }
+    set ValorB(value) {
+        this.valorB = value;
+    }
+    get Resultado() {
+        return this.resultado;
+    }
+}
+class Suma extends Operacion {
+    Sumar() {
+        this.resultado = this.valorA + this.valorB;
+    }
+}
+class Resta extends Operacion {
+    Restar() {
+        this.resultado = this.valorA - this.valorB;
+    }
+}
+let opercaionS = new Suma();
+opercaionS.ValorA = 45;
+opercaionS.ValorB = 10;
+opercaionS.Sumar();
+console.log("El resultado de la suma es " + opercaionS.Resultado);
+alert("El resultado de la suma es " + opercaionS.Resultado);
+let opercaionR = new Resta();
+opercaionR.ValorA = 45;
+opercaionR.ValorB = 10;
+opercaionR.Restar();
+console.log("El resultado de la resta es " + opercaionR.Resultado);
+alert("El resultado de la resta es " + opercaionR.Resultado);
+// Practica de Aereoplano
+class Turbina {
+    constructor(n) {
+        this.numTurbinas = 0;
+        this.numTurbinas = n;
+    }
+    ToString() {
+        return this.numTurbinas + " Turbina/s";
+    }
+}
+class Cubierta {
+    constructor(pCabinaTripulacion, pCabinaVuelo, pSistemaEmergencia, pTanquesCombustible, pPuertasSalida) {
+        this.cabinaTripulacion = false;
+        this.cabinaVuelo = false;
+        this.sistemaEmergencia = false;
+        this.numTanquesCombustible = 4;
+        this.numPuestasSalidas = 4;
+        this.cabinaTripulacion = pCabinaTripulacion;
+        this.cabinaVuelo = pCabinaVuelo;
+        this.sistemaEmergencia = pSistemaEmergencia;
+        this.numTanquesCombustible = pTanquesCombustible;
+        this.numPuestasSalidas = pPuertasSalida;
+    }
+    ToString() {
+        let mensaje = "Cubierta compuesta de: ";
+        if (this.cabinaVuelo) {
+            mensaje += " cubierta de Vuelo, ";
+        }
+        if (this.cabinaTripulacion) {
+            mensaje += " cubierta de Tripulación, ";
+        }
+        if (this.sistemaEmergencia) {
+            mensaje += " Sistema de Emergencia, ";
+        }
+        mensaje += this.numTanquesCombustible + " Tanques de Combustible, ";
+        mensaje += this.numPuestasSalidas + " Puertas de Salida. ";
+        return mensaje;
+    }
+}
+class Helice {
+    constructor(n) {
+        this.numHelices = 1;
+        this.numHelices = n;
+    }
+    ToString() {
+        return this.numHelices + "  Helices";
+    }
+}
+class Alas {
+    constructor(n) {
+        this.numAla = 2;
+        this.numAla = n;
+    }
+    ToString() {
+        return this.numAla + "  Las Alas";
+    }
+}
+class TrenDeAterrizaje {
+    constructor(n) {
+        this.numTrenAterrizaje = 3;
+        this.numTrenAterrizaje = n;
+    }
+    ToString() {
+        return this.numTrenAterrizaje + "  Los Trenes";
+    }
+}
+class Aereoplano {
+    constructor(phelice, pTrenAterrizaje, pAlas, pCubierta) {
+        this.helice = phelice;
+        this.trenAterrizaje = pTrenAterrizaje;
+        this.alas = pAlas;
+        this.cubierta = pCubierta;
+    }
+    ToString() {
+        let mensaje = " Aeroplano compuesto por: ";
+        mensaje += this.helice.ToString();
+        mensaje += this.alas.ToString();
+        mensaje += this.trenAterrizaje.ToString();
+        mensaje += this.cubierta.ToString();
+        return mensaje;
+    }
+}
